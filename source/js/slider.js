d9f8fa689002b6f726f0bd857a5b6a3b5b81911d
Viktor Rybakov <viktor.rybakov@gmail.com>
2021-05-12 18:57:11 +0300

    Исправляет слайдер

--- source/js/slider.js
+++ source/js/slider.js
@@ -27,62 +27,63 @@ document.addEventListener('DOMContentLoaded', () => {
         setTabletSlider();
       }
     });
-  }
 
-  function setMobileSlider() {
-    sliderImageWrapper.style.width = '100%';
-    sliderToggle.classList.add('slider__toggle--before');
-    sliderRange.value = '0';
-
-    sliderButtonAfter.onclick = (evt) => {
-      evt.preventDefault();
-
-      sliderToggle.classList.remove('slider__toggle--before');
-      sliderToggle.classList.add('slider__toggle--after');
-      sliderImageWrapper.style.width = '0%';
-    }
-
-    sliderButtonBefore.onclick = (evt) => {
-      evt.preventDefault();
-
-      sliderToggle.classList.remove('slider__toggle--after');
+    function setMobileSlider() {
+      sliderImageWrapper.style.width = '100%';
       sliderToggle.classList.add('slider__toggle--before');
-      sliderImageWrapper.style.width = '100%';
+      sliderRange.value = '0';
+
+      sliderButtonAfter.onclick = (evt) => {
+        evt.preventDefault();
+
+        sliderToggle.classList.remove('slider__toggle--before');
+        sliderToggle.classList.add('slider__toggle--after');
+        sliderImageWrapper.style.width = '0%';
+      }
+
+      sliderButtonBefore.onclick = (evt) => {
+        evt.preventDefault();
+
+        sliderToggle.classList.remove('slider__toggle--after');
+        sliderToggle.classList.add('slider__toggle--before');
+        sliderImageWrapper.style.width = '100%';
+      }
+    }
+
+    function setTabletSlider() {
+      sliderImageWrapper.style.width = '50%';
+      sliderToggle.classList.remove('slider__toggle--before');
+      sliderToggle.classList.remove('slider__toggle--after');
+      sliderRange.value = '50';
+
+      const sliderWidth = slider.offsetWidth;
+      const rangeWidth = sliderRange.offsetWidth - 34;
+
+      sliderRange.oninput = () => {
+        let wrapperWidth = (sliderWidth - rangeWidth) / 2 + (rangeWidth) * sliderRange.value / 100;
+        sliderImageWrapper.style.width = `${wrapperWidth}px`;
+        section.classList.remove('results--before');
+        section.classList.remove('results--after');
+      };
+
+      sliderButtonBefore.onclick = (evt) => {
+        evt.preventDefault();
+
+        sliderRange.value = '0';
+        sliderImageWrapper.style.width = '100%';
+        section.classList.add('results--before');
+        section.classList.remove('results--after');
+      };
+
+      sliderButtonAfter.onclick = (evt) => {
+        evt.preventDefault();
+
+        sliderRange.value = '100';
+        sliderImageWrapper.style.width = '0%';
+        section.classList.remove('results--before');
+        section.classList.add('results--after');
+      };
     }
   }
 
-  function setTabletSlider() {
-    sliderImageWrapper.style.width = '50%';
-    sliderToggle.classList.remove('slider__toggle--before');
-    sliderToggle.classList.remove('slider__toggle--after');
-    sliderRange.value = '50';
-
-    const sliderWidth = slider.offsetWidth;
-    const rangeWidth = sliderRange.offsetWidth - 34;
-
-    sliderRange.oninput = () => {
-      let wrapperWidth = (sliderWidth - rangeWidth) / 2 + (rangeWidth) * sliderRange.value / 100;
-      sliderImageWrapper.style.width = `${wrapperWidth}px`;
-      section.classList.remove('results--before');
-      section.classList.remove('results--after');
-    };
-
-    sliderButtonBefore.onclick = (evt) => {
-      evt.preventDefault();
-
-      sliderRange.value = '0';
-      sliderImageWrapper.style.width = '100%';
-      section.classList.add('results--before');
-      section.classList.remove('results--after');
-    };
-
-    sliderButtonAfter.onclick = (evt) => {
-      evt.preventDefault();
-
-      sliderRange.value = '100';
-      sliderImageWrapper.style.width = '0%';
-      section.classList.remove('results--before');
-      section.classList.add('results--after');
-    };
-  }
 });
